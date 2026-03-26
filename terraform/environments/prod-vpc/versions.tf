ffde4f5db78cb2ba5e915ff13d42ab022e7a9341
kwonjh0406 <kwonjh0406@gmail.com>
2026-03-26 19:58:17 +0900

    🐛 fix: EKS Terraform

--- terraform/environments/prod-vpc/versions.tf
+++ terraform/environments/prod-vpc/versions.tf
@@ -9,10 +9,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket     = "s3-an2-airline-tfstate-036333380579-ap-northeast-2-an"
-    key        = "prod-vpc/terraform.tfstate"
-    region     = "ap-northeast-2"
-    encrypt    = true
-    kms_key_id = "arn:aws:kms:ap-northeast-2:036333380579:key/40c16cfc-d284-4f40-a5ed-368bf54022a3"
+    bucket       = "s3-an2-airline-tfstate-036333380579-ap-northeast-2-an"
+    key          = "prod-vpc/terraform.tfstate"
+    region       = "ap-northeast-2"
+    encrypt      = true
+    use_lockfile = true
+    kms_key_id   = "arn:aws:kms:ap-northeast-2:036333380579:key/40c16cfc-d284-4f40-a5ed-368bf54022a3"
   }
 }
